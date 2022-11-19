8c01a82c9ea52485fb7902ee12e3cffdef2bfb16
jcmvdb <mvdb.jeroen@gmail.com>
2022-11-19 16:45:58 +0100

    Fixed Test command
    there was an typo made for the no option input given it was ?? not &&

--- commands/test.js
+++ commands/test.js
@@ -8,9 +8,8 @@ module.exports = {
             option.setName('input')
                 .setDescription('The input to echo back')),
     async execute(client, interaction) {
-        // await interaction.reply("het werk eindelijk");
-        const reason = 'Er is geen reden gegeven';
-        let test = interaction.options.getString('input') && reason;
+        const noOption = 'Er is geen reden gegeven';
+        let test = interaction.options.getString('input') ?? noOption;
         await interaction.reply(`het werk eindelijk ${interaction.user.username} en dit is de input **${test}**`);
         // console.log(test)
 
