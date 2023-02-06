f35bd7e8eda6cf27dbb0b159b98406063d2e0c23
Bas <62489234+Okayrug93@users.noreply.github.com>
2023-02-06 14:52:18 +0100

    improved embedCreator, changed catPictures to use embedCreator

--- commands/catPictures.js
+++ commands/catPictures.js
@@ -1,36 +1,34 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js")
+const embedCreator = require("../embedCreator")
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("cat")
         .setDescription("get a free cat pictrue"),
 
-    async execute(client, interaction, secret, embedCreator) {
+    async execute(client, interaction, secret) {
         const catApiURL = "https://api.thecatapi.com/v1/"
         const headers = {
             "x-api-key": `${secret.catKey}`,
             "Content-Type": "application/json",
         }
 
-        const embed = new EmbedBuilder()
-            .setTitle("Here a cat image")
-            .setDescription("A cat")
-            .setColor("#f1d02a")
-            .addFields( 
-                {name: "Bot name", value: client.user.username}
-            )
-            .setTimestamp()
-            .setFooter({text: "Image from https://thecatapi.com/"})
-            .setAuthor({name: interaction.member.user.username})
-        
-        //get a cat image
+        const embedDTO = {
+            title : "Here a cat image",
+            fields : [{
+                name : "Bot", value : client.user.username
+            }],
+            description : "A cat",
+            footer : "Image from https://thecatapi.com",
+            client,
+            interaction
+        }
+
         fetch(catApiURL.concat("images/search?format=json"), {"method" : "GET", headers})
-        .then( response => 
-             response.json()
-        ).then(json => 
-            interaction.reply({embeds: [EmbedBuilder.from(embed).setImage(json[0].url)]})  
-        );
-        
-        
+            .then( response => 
+                response.json()
+            ).then(json => 
+                interaction.reply({embeds: [embedCreator.createEmbed(embedDTO, "#f1d02a", json[0].url)]})  
+            );    
     },
 }
