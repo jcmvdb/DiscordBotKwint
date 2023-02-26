26a73fb5b053e31156849c58a9342c33f735297d
Bas <62489234+Okayrug93@users.noreply.github.com>
2023-02-26 12:17:00 +0100

    fixed unused arg in postApiData

--- src/utils/databaseFunctions.util.js
+++ src/utils/databaseFunctions.util.js
@@ -1,33 +1,35 @@
-const  { errorHandler } = require("./errorHandling.util")
-const secret = require("../../secrets/secrets.json");
+const { errorHandler } = require('./errorHandling.util');
+const secret = require('../../secrets/secrets.json');
 
 function sendData(path, interaction, data) {
-    const URL = "https://discord.jcmvdb.com/"
-    const queryString = Object.keys(data).map(key => key + '=' + data[key]).join('&').replaceAll(" ", "%20");
-    // console.log(data)
-    
-    const fetchURL = URL.concat(`${path}?${queryString}&key=${secret.key}`);
-    fetch(fetchURL)
-        .catch( err => errorHandler(err))
+	const URL = 'https://discord.jcmvdb.com/';
+	const queryString = Object.keys(data)
+		.map((key) => key + '=' + data[key])
+		.join('&')
+		.replaceAll(' ', '%20');
+	// console.log(data)
+
+	const fetchURL = URL.concat(`${path}?${queryString}&key=${secret.key}`);
+	fetch(fetchURL).catch((err) => errorHandler(err));
 }
 
 function getApiData(path) {
-    const url = `https://discord.jcmvdb.com/api/${path}`;
-    return fetch(url).then(async res => {
-        return res.json();
-    });
+	const url = `https://discord.jcmvdb.com/api/${path}`;
+	return fetch(url).then(async (res) => {
+		return res.json();
+	});
 }
 
-function postApiData(path, data, interaction) {
-    const url = `https://discord.jcmvdb.com/api/${path}`;
-    console.log(data);
-    fetch(url, {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    });
+function postApiData(path, data) {
+	const url = `https://discord.jcmvdb.com/api/${path}`;
+	console.log(data);
+	fetch(url, {
+		method: 'POST',
+		body: JSON.stringify(data),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+	});
 }
 
 module.exports = { sendData, getApiData, postApiData };
