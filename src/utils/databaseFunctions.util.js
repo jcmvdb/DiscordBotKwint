c19f08902ed9e3e2bcd6da54158eb6f61fed742e
jcmvdb <mvdb.jeroen@gmail.com>
2023-02-28 17:26:17 +0100

    removed sendData function
    old and unused function

--- src/utils/databaseFunctions.util.js
+++ src/utils/databaseFunctions.util.js
@@ -1,17 +1,5 @@
 const { errorHandler } = require('./errorHandling.util');
 
-function sendData(path, interaction, data) {
-	const URL = 'https://discord.jcmvdb.com/';
-	const queryString = Object.keys(data)
-		.map((key) => key + '=' + data[key])
-		.join('&')
-		.replaceAll(' ', '%20');
-	// console.log(data)
-
-	const fetchURL = URL.concat(`${path}?${queryString}&key=${process.env.DB_KEY}`);
-	fetch(fetchURL).catch((err) => errorHandler(err));
-}
-
 function getApiData(path) {
 	const url = `https://discord.jcmvdb.com/api/${path}`;
 	return fetch(url).then(async (res) => {
@@ -31,4 +19,4 @@ function postApiData(path, data) {
 	});
 }
 
-module.exports = { sendData, getApiData, postApiData };
+module.exports = { getApiData, postApiData };
