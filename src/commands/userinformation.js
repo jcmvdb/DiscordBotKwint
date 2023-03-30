c71da06b9364bd4bcf0c2655a6ad3f2c0732c121
Bas <62489234+Okayrug93@users.noreply.github.com>
2023-03-30 08:41:24 +0200

    rewrote userInformation

--- src/commands/userinformation.js
+++ src/commands/userinformation.js
@@ -5,83 +5,54 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
 		.setDescription('This give user information')
-		.addUserOption((option) =>
-			option.setName('user').setDescription('select a user')
-		),
+		.addUserOption((option) => option.setName('user').setDescription('select a user')),
 	async execute(client, interaction) {
-		const joinDate = new Date(interaction.member.joinedTimestamp).getDate();
-		const joinDay = new Date(interaction.member.joinedTimestamp).getDay();
-		const joinMonth = new Date(
-			interaction.member.joinedTimestamp
-		).getMonth();
-		const joinYear = new Date(
-			interaction.member.joinedTimestamp
-		).getFullYear();
+		const userOption = interaction.options.getUser('user');
+		const user = userOption ? userOption : interaction.member.user;
+		const createdDate = new Date(user.createdTimestamp);
+		const joinedDate = new Date((await interaction.guild.members.fetch(user)).joinedTimestamp);
+
 		const months = [
-			'jan',
-			'feb',
-			'mar',
-			'apr',
-			'may',
-			'jun',
-			'jul',
-			'aug',
-			'sept',
-			'oct',
-			'nov',
-			'dec',
+			'Jan',
+			'Feb',
+			'Mar',
+			'Apr',
+			'May',
+			'Jun',
+			'Jul',
+			'Aug',
+			'Sep',
+			'Oct',
+			'Nov',
+			'Dec',
 		];
-		const days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
-		const day = new Date(interaction.member.joinedTimestamp).getDay();
 
-		const noUser = '-';
-		const user = interaction.options.getUser('user') ?? noUser;
-
-		if (user === '-') {
-			const userEmbed = new EmbedBuilder()
-				.setTitle(
-					`${interaction.user.username}#${interaction.user.discriminator}`
-				)
-				.setDescription(`<@${interaction.user.id}>`)
-				.setColor('#443fac')
-				.addFields(
-					{ name: 'server', value: interaction.member.guild.name },
-					// {name: "Joined", value: joinDate + "/" + months[joinMonth] + "/" + joinYear},
-					{
-						name: 'Joined',
-						value: `${days[joinDay]}, ${months[joinMonth]} ${joinDate}, ${joinYear}`,
-					},
-					{ name: 'user selected', value: `${user.username}` }
-					// {name: "Registerd", value: date + "/" + months[month] + "/" + year}
-				)
-				.setThumbnail(`${interaction.user.displayAvatarURL()}`);
-			await interaction.reply({ embeds: [userEmbed] });
-		} else {
-			const joinDate = new Date(user.accentColor).getDate();
-			const joinDay = new Date(
-				interaction.member.joinedTimestamp
-			).getDay();
-			const joinMonth = new Date(
-				interaction.member.joinedTimestamp
-			).getMonth();
-			const joinYear = new Date(
-				interaction.member.joinedTimestamp
-			).getFullYear();
-			console.log(joinDate);
-
-			const userEmbed = new EmbedBuilder()
-				.setTitle(`${user.username}#${user.discriminator}`)
-				.setDescription(`<@${user.id}>`)
-				.setColor('#3fac7b')
-				.addFields(
-					{ name: 'server', value: interaction.member.guild.name },
-					{
-						name: 'Joined',
-						value: `${days[joinDay]}, ${months[joinMonth]} ${joinDate}, ${joinYear}`,
-					}
-				)
-				.setThumbnail(`${user.displayAvatarURL()}`);
-			await interaction.reply({ embeds: [userEmbed] });
-		}
+		const userEmbed = new EmbedBuilder()
+			.setTitle(`${user.username}#${user.discriminator}`)
+			.setDescription(`<@${user.id}>`)
+			.setColor('#3fac7b')
+			.addFields(
+				{
+					name: 'Server',
+					value: interaction.guild.name,
+				},
+				{
+					name: 'Joined on',
+					value: `${joinedDate.getDate()} \
+						${months[joinedDate.getMonth()]} \
+						${joinedDate.getFullYear()}`,
+					inline: true,
+				},
+				{
+					name: 'Created on',
+					value: `${createdDate.getDate()} \
+						${months[createdDate.getMonth()]} \
+						${createdDate.getFullYear()}`,
+					inline: true,
+				}
+			)
+			.setThumbnail(`${user.displayAvatarURL()}`);
+		await interaction.reply({ embeds: [userEmbed] });
 	},
 };
+//};
