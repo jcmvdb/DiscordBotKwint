d9cdfa27f3c56b61f905f003c4b55c93628d5010
Bas <62489234+Okayrug93@users.noreply.github.com>
2023-03-30 11:02:18 +0200

    Removed unnecessary imports in src/commands/index

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,36 +1,22 @@
-const apiGetData = require('./apiGetData');
-const apiPostData = require('./apiPostData');
-const array = require('./array');
 const ban = require('./ban');
 const kick = require('./kick');
-const botEmbed = require('./botEmbed');
 const catPictures = require('./catPictures');
 const giveRole = require('./giveRole');
-const role = require('./role');
 const makeProject = require('./makeProject');
 const makeQuote = require('./makeQuote');
 const getQuote = require('./getQuote');
 const rememberBirthDay = require('./rememberBirthday');
-const commands = require('./showCommands');
-const test = require('./test');
-const time = require('./time');
+const showCommands = require('./showCommands');
 const ping = require('./ping');
 const userInformation = require('./userInformation');
 
 module.exports = [
-	apiGetData,
-	apiPostData,
-	array,
 	ban,
-	botEmbed,
 	catPictures,
-	commands,
+	showCommands,
 	giveRole,
 	makeProject,
 	makeQuote,
-	role,
-	test,
-	time,
 	userInformation,
 	ping,
 	rememberBirthDay,
